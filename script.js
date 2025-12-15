9ebbbd2f7fd7a3c9882b9b9f42df10f4cef0ed89
Victoria Forte <victoria.pocladova@gmail.com>
2025-12-15 19:48:03 -0300

    feat:agregar primer sonido

--- script.js
+++ script.js
@@ -6,16 +6,19 @@ buttons.forEach(button => {
     button.addEventListener('click', function() {
         // Agregar animación
         this.classList.add('clicked');
-        
-        // Obtener el número del botón
-        const buttonNumber = this.getAttribute('data-sound');
-        
+
+        // Obtener el valor del atributo data-sound
+        const sound = this.getAttribute('data-sound');
+
         // Mostrar mensaje en consola
-        console.log(`Botón ${buttonNumber} presionado`);
-        
-        // Puedes agregar aquí la funcionalidad que desees
-        // Por ejemplo, reproducir un sonido, cambiar de página, etc.
-        
+        console.log(`Botón ${sound} presionado`);
+
+        // Si el botón es el de pedo, reproducir el sonido
+        if (sound === './sonidos/pedo.mp4') {
+            let audio = new Audio(sound);
+            audio.play();
+        }
+
         // Remover la clase de animación después de que termine
         setTimeout(() => {
             this.classList.remove('clicked');
